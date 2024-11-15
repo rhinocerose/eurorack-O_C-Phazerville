af4a45bcdbd79ade8faa624935f1087c94b4c454
Nicholas J. Michalek <djphazer@gmail.com>
2024-11-15 02:39:03 -0500

    Stereo version of Input applet with mix-to-mono

--- software/src/audio_applets/InputApplet.h
+++ software/src/audio_applets/InputApplet.h
@@ -4,34 +4,100 @@
 #include <Audio.h>
 #include "AudioIO.h"
 
-template <size_t Side> class InputApplet : public HemisphereAudioApplet {
+template <AudioChannels Channels>
+class InputApplet : public HemisphereAudioApplet {
 public:
   const char* applet_name() override {
-    return Side == 0 ? "Input L" : "Input R";
+    return Channels == MONO ? "Input" : "Inputs";
   }
   void Start() override {
-    conn.connect(OC::AudioIO::InputStream(), Side, amp, 0);
+    if (MONO == Channels) {
+      in_conn[0].connect(OC::AudioIO::InputStream(), 0, mixer[0], 0);
+      cross_conn[0].connect(OC::AudioIO::InputStream(), 1, mixer[0], 1);
+      in_conn[1].connect(input, 0, mixer[0], 3);
+
+      out_conn[0].connect(mixer[0], 0, output, 0);
+      mixer[0].gain(1, 0.0);
+    } else {
+      for (int i = 0; i < Channels; ++i) {
+        in_conn[i].connect(OC::AudioIO::InputStream(), i, mixer[i], 0);
+        cross_conn[i].connect(OC::AudioIO::InputStream(), i, mixer[1-i], 1);
+        in_conn[i+2].connect(input, i, mixer[i], 3);
+
+        out_conn[i].connect(mixer[i], 0, output, i);
+        mixer[i].gain(1, 0.0);
+      }
+    }
   }
   void Controller() override {}
-  void View() override {}
+  void View() override {
+    const char * const txt[] = {
+      "Left", "Right", "Mixed"
+    };
+    gfxPrint(2, 15, txt[mono_mode]);
+    if constexpr (Channels == STEREO) {
+      gfxPrint("+Right");
+
+      gfxPrint(2, 25, "Mix? ");
+      gfxPrint(2, 35, mixtomono ? "Mono" : "Stereo" );
+      gfxCursor(2, 44, 37);
+    } else {
+      gfxCursor(2, 24, 31);
+    }
+  }
   uint64_t OnDataRequest() override {
     return 0;
   }
   void OnDataReceive(uint64_t data) override {}
-  void OnEncoderMove(int direction) override {}
+  void OnEncoderMove(int direction) override {
+    if (!EditMode()) return;
+    if (Channels == MONO) {
+      mono_mode = constrain(mono_mode + direction, 0, MODE_COUNT-1);
+      if (mono_mode == MIXED) {
+        mixer[0].gain(0, 0.9);
+        mixer[0].gain(1, 0.9);
+      } else {
+        mixer[0].gain(mono_mode, 1.0);
+        mixer[0].gain(1 - mono_mode, 0.0);
+      }
+    } else {
+      mixtomono = !mixtomono;
+      mixer[0].gain(1, mixtomono? 1.0 : 0.0);
+      mixer[1].gain(1, mixtomono? 1.0 : 0.0);
+    }
+  }
 
   AudioStream* InputStream() override {
-    return nullptr;
+    return &input;
   }
   AudioStream* OutputStream() override {
-    return &amp;
+    return &output;
   }
 
 protected:
   void SetHelp() override {}
 
 private:
-  AudioAmplifier amp;
-  AudioConnection conn;
+  AudioPassthrough<Channels> input;
+  AudioConnection in_conn[Channels*2];
+  AudioConnection cross_conn[Channels];
+  AudioMixer4 mixer[Channels];
+  AudioConnection out_conn[Channels];
+  AudioPassthrough<Channels> output;
+
+  int mono_mode = LEFT;
+  bool mixtomono = 0;
+
+  enum SideMode {
+    LEFT, RIGHT,
+    MIXED,
+    MODE_COUNT
+  };
+  enum {
+    CHAN_SEL,
+    MIX_TOGGLE,
+    MAX_CURSOR = MIX_TOGGLE
+  };
+  int cursor;
 
 };
--- software/src/hemisphere_audio_config.h
+++ software/src/hemisphere_audio_config.h
@@ -13,14 +13,14 @@
 
 const size_t NUM_SLOTS = 5;
 
-std::tuple<InputApplet<0>, InputApplet<1>, UpsampledApplet> mono_input_pool[2];
-std::tuple<InputApplet<0>, InputApplet<1>> stereo_input_pool;
+std::tuple<InputApplet<MONO>, UpsampledApplet> mono_input_pool[2];
+std::tuple<InputApplet<STEREO>, WavPlayerApplet<STEREO>> stereo_input_pool;
 std::tuple<PassthruApplet<MONO>, OscApplet, DelayApplet, LadderApplet<MONO>, FilterFolderApplet<MONO>, VcaApplet<MONO>>
   mono_processors_pool[2][NUM_SLOTS - 1];
 std::tuple<PassthruApplet<STEREO>, LadderApplet<STEREO>, FilterFolderApplet<STEREO>, VcaApplet<STEREO>, WavPlayerApplet<STEREO>>
   stereo_processors_pool[NUM_SLOTS - 1];
 
-AudioAppletSubapp<NUM_SLOTS, 3, 2, 6, 5> audio_app(
+AudioAppletSubapp<NUM_SLOTS, 2, 2, 6, 5> audio_app(
   mono_input_pool,
   stereo_input_pool,
   mono_processors_pool,
