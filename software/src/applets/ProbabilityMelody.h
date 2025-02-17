c785d3ef0d4c727bed0d2f89651448127adf4d8e
beau-seidon <beau.seidon@outlook.com>
2025-02-17 02:47:21 -0600

    ProbMeloD: probability rotation (squashed)
    move encoder past the last note under the output indicator, and press to enable endless encoder weight rotation
    
    press AuxButton while editing any parameter to toggle override of CV range mod and enable CV weight rotation.
        1 full semitone-rotation is 5 volts.
        volt to positive-rotation rate depends on number of nonzero notes. if all are nonzero, same as semitone
    
    indicators for active enocoder and CV rotate modes are small nested rectangles shown under the output bar
    
    optimize; help text; save cv_rotate flag
    
    use SemitoneIn() for CV hysteresis
    
    Semitone rotation increment
    
    negative-weight rotation mask, bug fixes
    
    added negative-weight rotation mask.
    set weight to -1 to exclude note from CV2 rotation.
    
    added cv latch to keep range bounds from jumping when switching from cv rotate mode.
    sweep CV until it matches the value before switching modes, and parameter will be "re-captured"
    implemented for rotation as well, but didn't like the behavior so commented out.
    
    updated UI to hide faders when note is disabled.
    disabled notes show X instead of a weight
    
    minor styling cleanup, minor performance improvements
    
    tweak - no line when weight < 0

--- software/src/applets/ProbabilityMelody.h
+++ software/src/applets/ProbabilityMelody.h
@@ -1,4 +1,5 @@
 // Copyright (c) 2022, Benjamin Rosenbach
+// Modified (M) 2025, Beau Sterling
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -30,7 +31,8 @@ public:
     enum ProbMeloCursor {
         LOWER, UPPER,
         FIRST_NOTE = 2,
-        LAST_NOTE = 13
+        LAST_NOTE = 13,
+        ROTATE
     };
 
     const char* applet_name() {
@@ -47,17 +49,39 @@ public:
     void Controller() {
         loop_linker->RegisterMelo(hemisphere);
 
+        ForEachChannel(ch) { // prevent ranges from jumping to new values when changing mod modes
+            if (mod_latch[ch]) {
+                if (cv_rotate) {
+                    // if (abs(SemitoneIn(ch) - last_rot_cv[ch]) < 2) mod_latch[ch] = false;
+                } else {
+                    if (abs(SemitoneIn(ch) - last_range_cv[ch]) < 2) mod_latch[ch] = false;
+                }
+            }
+        }
+
         // stash these to check for regen
         int oldDown = down_mod;
         int oldUp = up_mod;
 
-        // CV modulation
-        down_mod = down;
-        up_mod = up;
-        // down scales to the up setting
-        Modulate(down_mod, 0, 1, up);
-        // up scales full range, with down value as a floor
-        Modulate(up_mod, 1, down_mod, HEM_PROB_MEL_MAX_RANGE);
+        if (cv_rotate && range_init) { // override original cv mod functions to rotate probabilities
+            ForEachChannel(ch) {
+                int last_rotation = rotation[ch];
+                rotation[ch] = SemitoneIn(ch);
+                int step = rotation[ch] - last_rotation;
+                if (ch && step) RotateMaskedWeights(weights, step);
+                else if (step) RotateAllWeights(weights, step);
+            }
+        } else { // CV modulation
+            if (!mod_latch[0]) {
+                down_mod = down;
+                Modulate(down_mod, 0, 1, up); // down scales to the up setting
+            }
+            if (!mod_latch[1]) {
+                up_mod = up;
+                Modulate(up_mod, 1, down_mod, HEM_PROB_MEL_MAX_RANGE); // up scales full range, with down value as a floor
+            }
+            range_init = true;
+        }
 
         // regen when looping was enabled from ProbDiv
         bool regen = loop_linker->IsLooping() && !isLooping;
@@ -65,7 +89,7 @@ public:
 
         // reseed from ProbDiv
         regen = regen || loop_linker->ShouldReseed();
-        
+
         // reseed loop if range has changed due to CV
         regen = regen || (isLooping && (down_mod != oldDown || up_mod != oldUp));
 
@@ -108,83 +132,91 @@ public:
 
     void OnEncoderMove(int direction) {
         if (!EditMode()) {
-            MoveCursor(cursor, direction, LAST_NOTE);
+            MoveCursor(cursor, direction, ROTATE);
             return;
         }
 
-        if (cursor >= FIRST_NOTE) {
+        if (cursor == ROTATE) {
+            RotateAllWeights(weights, direction);
+        } else if (cursor >= FIRST_NOTE) {
             // editing note probability
             int i = cursor - FIRST_NOTE;
-            weights[i] = constrain(weights[i] + direction, 0, HEM_PROB_MEL_MAX_WEIGHT);
+            weights[i] = constrain(weights[i] + direction, -1, HEM_PROB_MEL_MAX_WEIGHT); // -1 removes note from mask
             value_animation = HEMISPHERE_CURSOR_TICKS;
         } else {
             // editing scaling
-            if (cursor == LOWER) down = constrain(down + direction, 1, up);
-            if (cursor == UPPER) up = constrain(up + direction, down, 60);
+            if (cv_rotate) {
+                if (cursor == LOWER) down_mod = constrain(down_mod + direction, 1, up_mod);
+                if (cursor == UPPER) up_mod = constrain(up_mod + direction, down_mod, 60);
+            } else {
+                if (cursor == LOWER) down = constrain(down + direction, 1, up);
+                if (cursor == UPPER) up = constrain(up + direction, down, 60);
+            }
         }
         if (isLooping) {
             GenerateLoop(); // regenerate loop on any param changes
         }
     }
-        
+
+    void AuxButton() {
+        cv_rotate = !cv_rotate;
+        ForEachChannel(ch) { // prevent ranges from jumping to new values when changing mod modes
+            mod_latch[ch] = true;
+            if (cv_rotate) last_range_cv[ch] = SemitoneIn(ch);
+            // else last_rot_cv[ch] = SemitoneIn(ch);
+        }
+    }
+
     uint64_t OnDataRequest() {
         uint64_t data = 0;
-        Pack(data, PackLocation {0, 4}, weights[0]);
-        Pack(data, PackLocation {4, 4}, weights[1]);
-        Pack(data, PackLocation {8, 4}, weights[2]);
-        Pack(data, PackLocation {12, 4}, weights[3]);
-        Pack(data, PackLocation {16, 4}, weights[4]);
-        Pack(data, PackLocation {20, 4}, weights[5]);
-        Pack(data, PackLocation {24, 4}, weights[6]);
-        Pack(data, PackLocation {28, 4}, weights[7]);
-        Pack(data, PackLocation {32, 4}, weights[8]);
-        Pack(data, PackLocation {36, 4}, weights[9]);
-        Pack(data, PackLocation {40, 4}, weights[10]);
-        Pack(data, PackLocation {44, 4}, weights[11]);
+        for (size_t i = 0; i < 12; ++i) {
+            Pack(data, PackLocation {i*4, 4}, weights[i]+1);
+        }
         Pack(data, PackLocation {48, 6}, down);
         Pack(data, PackLocation {54, 6}, up);
+        Pack(data, PackLocation {60, 1}, cv_rotate);
         return data;
     }
 
     void OnDataReceive(uint64_t data) {
-        weights[0] = Unpack(data, PackLocation {0,4});
-        weights[1] = Unpack(data, PackLocation {4,4});
-        weights[2] = Unpack(data, PackLocation {8,4});
-        weights[3] = Unpack(data, PackLocation {12,4});
-        weights[4] = Unpack(data, PackLocation {16,4});
-        weights[5] = Unpack(data, PackLocation {20,4});
-        weights[6] = Unpack(data, PackLocation {24,4});
-        weights[7] = Unpack(data, PackLocation {28,4});
-        weights[8] = Unpack(data, PackLocation {32,4});
-        weights[9] = Unpack(data, PackLocation {36,4});
-        weights[10] = Unpack(data, PackLocation {40,4});
-        weights[11] = Unpack(data, PackLocation {44,4});
-        down = Unpack(data, PackLocation{48,6});
-        up = Unpack(data, PackLocation{54,6});
+        for (size_t i = 0; i < 12; ++i) {
+            weights[i] = Unpack(data, PackLocation {i*4,4})-1;
+        }
+        down = constrain(Unpack(data, PackLocation{48,6}), 1, up);
+        up = constrain(Unpack(data, PackLocation{54,6}), down, 60);
+        cv_rotate = Unpack(data, PackLocation{60,1});
+
+        if (cv_rotate) range_init = false;
     }
 
 protected:
-  void SetHelp() {
-    //                    "-------" <-- Label size guide
-    help[HELP_DIGITAL1] = "Clock 1";
-    help[HELP_DIGITAL2] = "Clock 2";
-    help[HELP_CV1]      = "Lower";
-    help[HELP_CV2]      = "Upper";
-    help[HELP_OUT1]     = "Pitch 1";
-    help[HELP_OUT2]     = "Pitch 2";
-    help[HELP_EXTRA1] = "Set: Range bounds /";
-    help[HELP_EXTRA2] = "     Note weights";
-    //                  "---------------------" <-- Extra text size guide
-  }
+    void SetHelp() {
+        //                    "-------" <-- Label size guide
+        help[HELP_DIGITAL1] = "Clock 1";
+        help[HELP_DIGITAL2] = "Clock 2";
+        help[HELP_CV1]      = cv_rotate? "RotKey" : "Lower";
+        help[HELP_CV2]      = cv_rotate? "RotMask" : "Upper";
+        help[HELP_OUT1]     = "Pitch 1";
+        help[HELP_OUT2]     = "Pitch 2";
+        help[HELP_EXTRA1]   = "Set: Range bounds /";
+        help[HELP_EXTRA2]   = "     Note weights";
+        //                    "---------------------" <-- Extra text size guide
+    }
 
 private:
-    int cursor; // ProbMeloCursor 
-    int weights[12] = {10,0,0,2,0,0,0,2,0,0,4,0};
+    int cursor; // ProbMeloCursor
+    int8_t weights[12] = {10,0,0,2,0,0,0,2,0,0,4,0};
     int up, up_mod;
     int down, down_mod;
     int pitch;
     bool isLooping = false;
     int seqloop[2][HEM_PROB_MEL_MAX_LOOP_LENGTH];
+    bool cv_rotate = false;
+    int8_t rotation[2] = {0};
+    bool mod_latch[2] = {false};
+    // int8_t last_rot_cv[2];
+    int8_t last_range_cv[2];
+    bool range_init = false;
 
     ProbLoopLinker *loop_linker = loop_linker->get();
 
@@ -194,16 +226,53 @@ private:
     const uint8_t p[12] = {0, 1,  0,  1,  0,  0,  1,  0,  1,  0,  1,  0};
     const char* n[12] = {"C", "C", "D", "D", "E", "F", "F", "G", "G", "A", "A", "B"};
 
+    void RotateAllWeights(int8_t weights[], int r) {
+        if (r == 0) return;
+        r = r % 12;
+        if (r < 0) r += 12;
+
+        int8_t temp[12];
+        for (int i = 0; i < 12; ++i) {
+            temp[i] = weights[i];
+        }
+
+        for (int i = 0; i < 12; ++i) {
+            weights[(i + r) % 12] = temp[i];
+        }
+    }
+
+    void RotateMaskedWeights(int8_t weights[], int r) {
+        if (r == 0) return;
+
+        int count = 0;
+        uint8_t indices[12];
+        int8_t values[12];
+        for (int i = 0; i < 12; ++i) {
+            if (weights[i] >= 0) {
+                indices[count] = i;
+                values[count] = weights[i];
+                ++count;
+            }
+        }
+        if (count == 0) return;
+
+        r = r % count;
+        if (r < 0) r += count;
+        for (int i = 0; i < count; ++i) {
+            weights[indices[(i + r) % count]] = values[i];
+        }
+    }
+
     int GetNextWeightedPitch() {
         int total_weights = 0;
 
-        for(int i = down_mod-1; i < up_mod; i++) {
-            total_weights += weights[i % 12];
+        for(int i = down_mod-1; i < up_mod; ++i) {
+            total_weights += max(0, weights[i % 12]);
         }
 
         int rnd = random(0, total_weights + 1);
-        for(int i = down_mod-1; i < up_mod; i++) {
-            int weight = weights[i % 12];
+        for(int i = down_mod-1; i < up_mod; ++i) {
+            int weight = max(0, weights[i % 12]);
             if (rnd <= weight && weight > 0) {
                 return i;
             }
@@ -214,7 +283,7 @@ private:
 
     void GenerateLoop() {
         // always fill the whole loop to make things easier
-        for (int i = 0; i < HEM_PROB_MEL_MAX_LOOP_LENGTH; i++) {
+        for (int i = 0; i < HEM_PROB_MEL_MAX_LOOP_LENGTH; ++i) {
             seqloop[0][i] = GetNextWeightedPitch();
             seqloop[1][i] = GetNextWeightedPitch();
         }
@@ -225,8 +294,7 @@ private:
         gfxFrame(0, 27, 63, 32);
 
         // White keys
-        for (uint8_t x = 0; x < 8; x++)
-        {
+        for (uint8_t x = 0; x < 8; ++x) {
             if (x == 3 || x == 7) {
                 gfxLine(x * 8, 27, x * 8, 58);
             } else {
@@ -235,8 +303,7 @@ private:
         }
 
         // Black keys
-        for (uint8_t i = 0; i < 6; i++)
-        {
+        for (uint8_t i = 0; i < 6; ++i) {
             if (i != 2) { // Skip the third position
                 uint8_t x = (i * 8) + 6;
                 gfxInvert(x, 28, 5, 15);
@@ -248,10 +315,10 @@ private:
         int note = pitch % 12;
         int octave = (pitch - 60) / 12;
 
-        for (uint8_t i = 0; i < 12; i++)
-        {
+        for (uint8_t i = 0; i < 12; ++i) {
             uint8_t xOffset = x[i] + (p[i] ? 1 : 2);
             uint8_t yOffset = p[i] ? 31 : 45;
+            bool unmasked = (weights[i] >= 0);
 
             if (pulse_animation > 0 && note == i) {
                 gfxRect(xOffset - 1, yOffset, 3, 10);
@@ -263,20 +330,33 @@ private:
                     } else {
                         gfxDottedLine(xOffset, yOffset, xOffset, yOffset + 10);
                     }
-                } else {
-                    gfxDottedLine(xOffset, yOffset, xOffset, yOffset + 10);    
+                } else if (unmasked) {
+                    gfxDottedLine(xOffset, yOffset, xOffset, yOffset + 10);
                 }
-                gfxLine(xOffset - 1, yOffset + 10 - weights[i], xOffset + 1, yOffset + 10 - weights[i]);
+                if (unmasked)
+                  gfxLine(xOffset - 1, yOffset + 10 - weights[i], xOffset + 1, yOffset + 10 - weights[i]);
             }
         }
 
         // cursor for keys
         if (!EditMode()) {
-            if (cursor >= FIRST_NOTE) {
+            if (cursor == ROTATE) {
+                gfxCursor(56, 60, 7);
+                gfxCursor(56, 61, 7);
+            } else if (cursor >= FIRST_NOTE) {
                 int i = cursor - FIRST_NOTE;
-                gfxCursor(x[i] - 1, p[i] ? 24 : 60, p[i] ? 5 : 6);
-                gfxCursor(x[i] - 1, p[i] ? 25 : 61, p[i] ? 5 : 6);
+                gfxCursor(x[i] - 1, p[i] ? 24 : 60, p[i] ? 5 : 7);
+                gfxCursor(x[i] - 1, p[i] ? 25 : 61, p[i] ? 5 : 7);
             }
+        } else {
+            if (cursor == ROTATE) {
+                gfxRect(56, 60, 7, 4);
+                gfxClear(57, 61, 5, 2);
+            }
+        }
+
+        if (cv_rotate) {
+            gfxRect(57, 61, 5, 2);
         }
 
         // scaling params
@@ -295,25 +375,26 @@ private:
         if (cursor == UPPER) gfxCursor(39, 23, 21);
 
         if (pulse_animation > 0) {
-        //     int note = pitch % 12;
-        //     int octave = (pitch - 60) / 12;
+            // int note = pitch % 12;
+            // int octave = (pitch - 60) / 12;
 
-        //     gfxRect(x[note] + (p[note] ? 0 : 1), p[note] ? 29 : 54, 3, 2);
+            // gfxRect(x[note] + (p[note] ? 0 : 1), p[note] ? 29 : 54, 3, 2);
             gfxRect(58, 54 - (octave * 6), 3, 3);
         }
 
         if (value_animation > 0 && cursor >= FIRST_NOTE) {
-          int i = cursor - FIRST_NOTE;
+            int i = cursor - FIRST_NOTE;
 
-          gfxRect(1, 15, 60, 10);
-          gfxInvert(1, 15, 60, 10);
+            gfxRect(1, 15, 60, 10);
+            gfxInvert(1, 15, 60, 10);
 
-          gfxPrint(18, 16, n[i]);
-          if (p[i]) {
-            gfxPrint(24, 16, "#");
-          }
-          gfxPrint(34, 16, weights[i]);
-          gfxInvert(1, 15, 60, 10);
+            gfxPrint(18, 16, n[i]);
+            if (p[i]) {
+                gfxPrint(24, 16, "#");
+            }
+            if (weights[i] < 0) gfxPrint(34, 16, "X");
+            else gfxPrint(34, 16, weights[i]);
+            gfxInvert(1, 15, 60, 10);
         }
     }
 };
