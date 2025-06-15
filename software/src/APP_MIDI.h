00f383f8967d1ffef3d167bd016462b35cdd2fe6
Nicholas J. Michalek <djphazer@gmail.com>
2025-06-15 18:08:40 -0400

    IOFrame and MIDI Refactoring
    - moving implementation out of headers
    - preparing to move MIDI processing outside of individual Apps
    + MIDI logging refactor

--- software/src/APP_MIDI.h
+++ software/src/APP_MIDI.h
@@ -895,7 +895,7 @@ private:
             // Handle system exclusive dump for Setup data
             if (message == HEM_MIDI_SYSEX) OnReceiveSysEx();
 
-            HS::frame.MIDIState.ProcessMIDIMsg(channel, message, data1, data2);
+            HS::frame.MIDIState.ProcessMIDIMsg({channel, message, data1, data2});
             //old_process_midi_in(message, channel, data1, data2);
         }
     }
